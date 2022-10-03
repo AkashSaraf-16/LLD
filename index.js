76733973a7e6003cc4a68fddabbe549f735755ef
Akash <akashsaraf24@gmail.com>
2022-10-03 15:31:14 +0530

    Added Single resposibility using module implementation

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("hi")
+// Single responsibilty :
+import generateResult from "./verdict.js"
+class Result {
+  constructor(name, marks) {
+    this.name = name
+    this.marks = marks
+    this.finalVerdict = "NA"
+  }
+  addMarks(score) {
+    this.marks += score
+    console.log(`Total marks uptill now ${this.marks}`)
+  }
+}
+
+const student1 = new Result("Ravi", 340)
+student1.addMarks(300)
+generateResult(student1.marks)
+
+// the above example is clearly violating the single responsilblity principle as here the
+// class Result has 2 reasons two change 1) if we change the addMarks logic 2) when we change
+// the generateResult logic. How to solve? Simply make a dedicated class to handle those
+// functionality
+
+// Confusion clearance regarding the 'this'
+// let group = {
+//   title: "Our Group",
+//   students: ["John", "Pete", "Alice"],
+
+//   showList() {
+//     this.students.forEach(function (student) {
+//       // Error: Cannot read property 'title' of undefined
+//       console.log(this, this.title + ": " + student)
+//     })
+//   },
+// }
+
+// group.showList()
---
+++ verdict.js
@@ -0,0 +1,4 @@
+export default function generateResult(marks) {
+  if (marks > 500) console.log("Yeppy! You have passed the exam!!")
+  else console.log("Oh boy! You have failed the exam!!")
+}
